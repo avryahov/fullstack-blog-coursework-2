912503e6a058012d8d290e9b6a29d384ede6d2a3
avrjakhov <a.v.rjakhov@yandex.ru>
2026-03-11 01:28:37 +0500

    Revert "feat: merge feature/posts-create into dev"
    This reverts commit 133e91eed6e40575c1a6bf55300c514f2012a9d8, reversing
    changes made to 633c7f4182988c66bf639660e61ffd73a046696d.

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,4 +1,5 @@
-import { createPostItem, getPostById, getPostsList } from '../services/post.service.js';
+import { getPostsList } from '../services/post.service.js';
+import { getPostById } from '../services/post.service.js';
 
 export const getPosts = async (req, res, next) => {
   try {
@@ -18,18 +19,6 @@ export const getPost = async (req, res, next) => {
     const post = await getPostById(req.params.id);
 
     res.status(200).json({
-      post,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const createPost = async (req, res, next) => {
-  try {
-    const post = await createPostItem(req.body);
-
-    res.status(201).json({
       post,
     });
   } catch (error) {
--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -1,14 +1,11 @@
 import { Router } from 'express';
-import { createPost, getPost, getPosts } from '../controllers/post.controller.js';
+import { getPost, getPosts } from '../controllers/post.controller.js';
 import { createComment } from '../controllers/comment.controller.js';
-import { authorize } from '../middleware/authorize.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { validateCommentBody } from '../validation/comment.validation.js';
-import { validatePostBody } from '../validation/post.validation.js';
 
 export const postRouter = Router();
 
 postRouter.get('/', getPosts);
 postRouter.get('/:id', getPost);
-postRouter.post('/', authenticate, authorize(['admin']), validatePostBody, createPost);
 postRouter.post('/:id/comments', authenticate, validateCommentBody, createComment);
