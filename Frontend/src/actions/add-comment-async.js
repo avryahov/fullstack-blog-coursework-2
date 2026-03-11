2a2077b38d3affd51e7874deb4cefd68007fe62c
avrjakhov <a.v.rjakhov@yandex.ru>
2026-03-11 12:15:03 +0500

    feature/frontend-parity-check: align async actions with domain api

--- Frontend/src/actions/add-comment-async.js
+++ Frontend/src/actions/add-comment-async.js
@@ -1,7 +1,7 @@
 import { setPostData } from './set-post-data';
 
 export const addCommentAsync = (requestServer, postId, content) => dispatch => {
-  requestServer('addComment', postId, content).then(postData => {
+  requestServer(postId, content).then(postData => {
     if (postData.res) {
       dispatch(setPostData(postData.res));
     }
--- Frontend/src/actions/remove-comment-async.js
+++ Frontend/src/actions/remove-comment-async.js
@@ -1,5 +1,9 @@
 import { setPostData } from './set-post-data';
 
 export const removeCommentAsync = (requestServer, postId, id) => dispatch => {
-  requestServer('removePostComment', postId, id).then(postData => dispatch(setPostData(postData.res)));
+  requestServer(postId, id).then(postData => {
+    if (postData.res) {
+      dispatch(setPostData(postData.res));
+    }
+  });
 };
