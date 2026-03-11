3cda3696cb634af7fc2eeba84f780d5bf4f88d50
avrjakhov <a.v.rjakhov@yandex.ru>
2026-03-11 11:22:19 +0500

    feature/frontend-api-dto-endpoints: normalize server request contract

--- Frontend/src/hooks/use-server-request.js
+++ Frontend/src/hooks/use-server-request.js
@@ -2,15 +2,14 @@ import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUserSession } from '../actions';
 import { server } from '../api';
+import { isPublicOperation } from '../api/operations';
 
 export const useServerRequest = () => {
   const session = useSelector(selectUserSession);
 
   return useCallback(
     (operation, ...params) => {
-      const request = ['register', 'authorize', 'fetchPost', 'fetchPosts'].includes(operation)
-        ? params
-        : [session, ...params];
+      const request = isPublicOperation(operation) ? params : [session, ...params];
 
       return server[operation](...request);
     },
